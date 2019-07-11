f71a9dfa3e874a1c3c0b7435bc8b3cad697e9add
WesleyASC6 <wesleytrujillo@uagateway.org>
2019-07-11 14:28:36 -0400

    project

---
+++ randomness.js
@@ -0,0 +1,50 @@
+//////////////////////
+// GLOBAL VARIABLES //
+//////////////////////
+const multiplier = 21;
+
+function number() {
+    const decimal=Math.random();
+    const times10 = decimal * multiplier;
+    const final = Math.floor (times10);
+    return final
+
+}
+console.log(number());
+
+const alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+function Letter () {
+    const letter =Math.random();
+    const times10 = letter * alphabet;
+    const final = decimal (times10)
+    return final
+}
+console.log(alphabet());
+
+
+
+
+
+
+//const foods = [
+
+  //  "pizza",
+   // "burgers",
+   // "rice and beans",
+  //  "lasagna",
+  //  "macaroni and cheese",
+  //  "baked potatoes"
+//];
+//const students = [
+  //  "Bob",
+   // "Martin",
+    //"Danny",
+   // "Mahdi",
+  //  "Harry",
+    //"Sean",
+   // "Robert"
+//];
+/////////////////////////////
+// END OF GLOBAL VARIABLES //
+/////////////////////////////
